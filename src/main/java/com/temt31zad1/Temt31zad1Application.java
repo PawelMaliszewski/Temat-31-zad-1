25e6d9d9d3d2e3610f0dd92d5f9a1ba0a9ab6122
Pawel Maliszewski <pablo999455@gmail.com>
2023-10-29 20:43:57 +0100

    master

---
+++ src/main/java/com/temt31zad1/Temt31zad1Application.java
@@ -0,0 +1,13 @@
+package com.temt31zad1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Temt31zad1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Temt31zad1Application.class, args);
+    }
+
+}
